05d6eb2523cd1fdf9d1c1872a331ec6cfac76d68
vishnu4486 <biradarvishnu89@gmail.com>
2020-06-24 10:22:37 +0530

    first time

---
+++ src/main/java/com/yash/ppa/PpaApplication.java
@@ -0,0 +1,14 @@
+package com.yash.ppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpaApplication.class, args);
+		System.out.println("Wel ome to the Function");
+	}
+
+}
